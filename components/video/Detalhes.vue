7e1641c33bbf7349c320552f21d039853af1272d
cotete <feliperibeirots@gmail.com>
2024-10-02 13:30:26 -0300

    composables

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>Components</h1>
+    <p>{{ diferencaEmDias }} dias atrás</p>
 </template>
+
+<script setup lang="ts">
+
+    const {diasPassados, diferencaEmDias} = useFormatacao();
+
+    const dataVideo = new Date(2023,9,2);
+
+    diasPassados(dataVideo)
+</script>
